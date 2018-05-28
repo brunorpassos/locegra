da6c74eb42f295c601c443f3a27c170c783b5b63
Bruno Passos <bruno.passos@creditas.com.br>
2018-05-27 22:10:53 -0300

    Inserting materialize

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    sassOptions: {
-      includePaths: [
-        'node_modules/bootstrap/scss'
-      ]
-}
 // Add options here
   });
 
